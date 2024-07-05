f34a3d445df671f656a92944c50e3d28cb464725
Kyaw Zin <kyawzinaung.186380@gmail.com>
2024-07-06 00:36:35 +0630

    php mvc oop

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'invoice');
+
+// Define App Root
+define ('APPROOT', dirname(dirname(__FILE__)));
+
+// Define URL Root
+define ('URLROOT', 'http://localhost/invoice');
+
+// Define SITENAME
+define('SITENAME', 'Invoice');
---
+++ app/controllers/Pages.php
@@ -0,0 +1,43 @@
+<?php
+
+class Pages extends Controller
+{
+    private $db;
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function index()
+    {
+        // echo('ok');
+        // exit;
+        $this->view('pages/login');
+    }
+
+    public function login()
+    {
+        $this->view('pages/login');
+    }
+
+    public function register()
+    {
+        $this->view('pages/register');
+    }
+
+    public function about()
+    {
+        $this->view('pages/about');
+    }
+
+    public function dashboard()
+    {
+        $income = $this->db->incomeTransition();
+        $expense = $this->db->expenseTransition();
+        $data = [
+            'income' => $income,
+            'expense' => $expense
+        ];
+        $this->view('pages/dashboard', $data);
+    }
+}
